2f93a3a42c936e81e4f0daf77de0d0fcabe6da2d
Leonardo Anjos <leodrums11@gmail.com>
2023-07-09 16:56:51 -0300

    products grpc controller

---
+++ src/products/products-grpc-server/products-grpc-server.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ProductsGrpcServerController } from './products-grpc-server.controller';
+
+describe('ProductsGrpcServerController', () => {
+  let controller: ProductsGrpcServerController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ProductsGrpcServerController],
+    }).compile();
+
+    controller = module.get<ProductsGrpcServerController>(ProductsGrpcServerController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { ProductsGrpcServerController } from './products-grpc-server/products-grpc-server.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
-  controllers: [ProductsController],
+  controllers: [ProductsController, ProductsGrpcServerController],
   providers: [ProductsService],
 })
 export class ProductsModule {}
