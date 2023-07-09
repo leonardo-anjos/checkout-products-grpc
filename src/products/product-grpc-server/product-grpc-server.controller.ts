4b2215bf39d336eab2b555572004e691f585ab70
Leonardo Anjos <leodrums11@gmail.com>
2023-07-09 17:25:15 -0300

    using service api rest on crontroller grpc

--- src/products/product-grpc-server/product-grpc-server.controller.ts
+++ src/products/product-grpc-server/product-grpc-server.controller.ts
@@ -1,12 +1,16 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { Metadata, ServerUnaryCall } from 'grpc';
+import { ProductsService } from '../products.service';
+import { CreateProductDto } from '../dto/create-product.dto';
 
 @Controller()
 export class ProductGrpcServerController {
+  constructor(private productsService: ProductsService);
+
   @GrpcMethod('ProductService', 'Create')
-  create(data, metadata: Metadata, call: ServerUnaryCall<any>) {
+  create(data: CreateProductDto, metadata: Metadata, call: ServerUnaryCall<CreateProductDto>) {
     console.log('data - ProductGrpcServerController', data);
-    return { id: 1, ...data };
+    return this.productsService.create(data);
   }
 }
