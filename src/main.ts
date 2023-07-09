a05c9231048c0071341a060d6fa55bc8e753c9c4
Leonardo Anjos <leodrums11@gmail.com>
2023-07-09 16:44:29 -0300

    grpc config and server intance

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EntityNotFoundExceptionFilter } from './exception-filters/entity-not-found.exception-filter';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new EntityNotFoundExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      url: '0.0.0.0:50051',
+      package: 'products',
+      protoPath: join(__dirname, 'products/proto/products.proto')
+    }
+  });
+
+  app.startAllMicroservices();
 
   await app.listen(3000);
 }
