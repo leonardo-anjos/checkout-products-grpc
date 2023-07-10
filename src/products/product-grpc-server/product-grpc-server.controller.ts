1e7b0d10950404cd65570662dbf0bfb0338cdbff
Leonardo Anjos <leodrums11@gmail.com>
2023-07-09 23:00:28 -0300

    finish crud with grpc

--- src/products/product-grpc-server/product-grpc-server.controller.ts
+++ src/products/product-grpc-server/product-grpc-server.controller.ts
@@ -1,16 +1,39 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { Metadata, ServerUnaryCall } from 'grpc';
-import { ProductsService } from '../products.service';
 import { CreateProductDto } from '../dto/create-product.dto';
+import { ProductsService } from '../products.service';
 
 @Controller()
 export class ProductGrpcServerController {
   constructor(private productsService: ProductsService);
 
-  @GrpcMethod('ProductService', 'Create')
+  @GrpcMethod('ProductService')
   create(data: CreateProductDto, metadata: Metadata, call: ServerUnaryCall<CreateProductDto>) {
-    console.log('data - ProductGrpcServerController', data);
     return this.productsService.create(data);
+  }
+
+  @GrpcMethod('ProductService')
+  update(data: { id: number, name: string, price: number }, metadata: Metadata, call: ServerUnaryCall<CreateProductDto>) {
+    const { id, ...rest } = data;
+    return this.productsService.update(id, rest);
+  }
+
+  @GrpcMethod('ProductService')
+  findOne(data: { id: number }) {
+    const { id } = data;
+    return this.productsService.findOne(id);
+  }
+
+  @GrpcMethod('ProductService')
+  async findAll(data) {
+    const products = await this.productsService.findAll();
+    return { data: products };
+  }
+
+  @GrpcMethod('ProductService')
+  remove(data: { id: number }) {
+    const { id } = data;
+    this.productsService.remove(id);
   }
 }
